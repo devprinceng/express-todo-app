8ec345bdb87ae654f3b5aebd65224f3446a3fc99
devprinceng <devprinceng@gmail.com>
2024-08-05 19:46:43 +0100

    code modified to use imported todoRouter

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const moment = require('moment');
 const app = express()
 const connectMongoDb = require('./init/MongoDb');
-const Todo = require('./models/Todo');
+const todoRouter = require('./routes/todoRouter');
 dotenv.config();
 const PORT = process.env.PORT || 3000
 
@@ -22,66 +22,8 @@ app.use('/js',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/j
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')))
 app.use(bodyParser.urlencoded({extended:true}));
+//use todoRouter
+app.use('/', todoRouter);
 
-//home route
-app.get('/', async (req, res, next) => {
-    try {
-        const title = 'All Todos'
-        const todos = await Todo.find({}).sort({createdAt: -1});
-        res.locals.moment = moment;
-        res.render('index', {title, todos})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-    
-})
-//newtodo route
-app.get('/newtodo', (req, res, next) => {
-    try {
-        const title = 'New Todo'
-        res.render('newtodo', {title})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-//edit todo route
-app.get('/edit-todo', (req, res, next) => {
-    try {
-        const title = 'Edit Todo'
-        res.render('edit-todo', {title})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-//delete todo route
-app.get('/delete-todo', (req, res, next) => {
-    try {
-        const title = 'Delete Todo'
-        res.render('delete-todo', {title})
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-// add new student post route
-app.post('/create-todo', async(req, res, next) => {
-    try {
-        console.log(req.body);
-        
-        const {title, description} = req.body;
-        if(!title){
-           return res.status(400).json({message: "Todo title is required"});
-        }
-        
-        const todo = await Todo.create({
-            title,
-            description,
-        });
-        if (todo){
-            return res.redirect('/');
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
 //listen on our server
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
