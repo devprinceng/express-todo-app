873e501ef527281e8752c5191638c6b5bbdf1e3f
devprinceng <devprinceng@gmail.com>
2024-08-05 21:58:22 +0100

    final refacto: moved app configurations to app module

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+const bodyParser = require('body-parser');
+const app = express()
+const connectMongoDb = require('./init/MongoDb');
+const todoRouter = require('./routes/todoRouter');
+dotenv.config();
+
+//connect Database
+connectMongoDb();
+
+//set view engine
+app.set('view engine', 'ejs');
+//serve static files from public directory
+app.use(express.static(path.join(__dirname,'public')));
+//serve static files from node_modules
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')))
+app.use(bodyParser.urlencoded({extended:true}));
+//use todoRouter
+app.use('/', todoRouter);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,28 +1,5 @@
-const express = require('express')
-const path = require('path')
-const dotenv = require('dotenv')
-const bodyParser = require('body-parser');
-const app = express()
-const connectMongoDb = require('./init/MongoDb');
-const todoRouter = require('./routes/todoRouter');
-dotenv.config();
+const app = require('./app')
 const PORT = process.env.PORT || 3000
-
-//connect Database
-connectMongoDb();
-
-//set view engine
-app.set('view engine', 'ejs');
-//serve static files from public directory
-app.use(express.static(path.join(__dirname,'public')));
-//serve static files from node_modules
-app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js',express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')))
-app.use(bodyParser.urlencoded({extended:true}));
-//use todoRouter
-app.use('/', todoRouter);
 
 //listen on our server
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
